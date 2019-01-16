a372e34f6a5eac643536c00fd920ff416c7466bb
Ronaldo Bini Jr <rbsbini@gmail.com>
2019-01-16 11:53:22 -0200

    creation simireports official

---
+++ simireports/simireports/simireports/Conn/BancoAzure.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    public class BancoAzure
+    {
+
+        public SqlConnection abrir()
+        {
+            string connetionString = null;
+            SqlConnection conn;
+            connetionString = "Data Source=similar.database.windows.net;Initial Catalog=SimiSysDB;User ID=similar;Password=@lbertor!ter15";
+            conn = new SqlConnection(connetionString);
+            try
+            {
+                conn.Open();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERRO AO CONECTAR COM BANCO AZURE" + ex);
+            }
+            return conn;
+
+        }
+
+
+        public void fechar(SqlConnection conn)
+        {
+
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERRO AO DAR CLOSE COM BANCO AZURE" + ex);
+            }
+
+        }
+
+
+
+        public string consultar(string sql)
+        {
+            string result = "-";
+            SqlConnection conn = abrir();
+            try
+            {
+                SqlCommand command = new SqlCommand(sql, conn);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    result = (string)reader[0];
+                }
+            }
+            catch (Exception e)
+            {
+                result = "ERRO: " + e;
+            }
+            fechar(conn);
+            return result;
+        }
+
+
+
+
+        public string executar(string sql)
+        {
+            string result = "-";
+            SqlConnection conn = abrir();
+            try
+            {
+                SqlCommand command =
+                    new SqlCommand(sql, conn);
+                command.ExecuteNonQuery();
+                result = "OK";
+            }
+            catch (Exception e)
+            {
+                result = "ERRO: " + e;
+            }
+            fechar(conn);
+            return result;
+        }
+
+
+
+
+    }
---
+++ simireports/simireports/simireports/index.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace simireports
+{
+    public partial class Index : System.Web.UI.Page
+    {
+
+        public string result = "-";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            result = new BancoAzure().consultar("SELECT nome FROM Usuarios");
+            Console.WriteLine(result);
+        }
+    }
+}
