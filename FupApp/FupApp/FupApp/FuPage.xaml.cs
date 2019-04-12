009144dc122c184fade42d67a3a0f95b9cfc1722
Verkans <victor@setupnet.com.br>
2019-04-12 17:40:00 -0300

    FupApp
    Inclusao do App para FUP no repositorio git

---
+++ FupApp/FupApp/FupApp/FuPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using FupApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FupApp
+{
+    public partial class FuPage : ContentPage
+    {
+        private Picker picker;
+        private string login;
+        private int count = 0;
+        private string erro;
+        private string quem;
+        private string sup;
+        //private string prop;
+
+        public FuPage()
+        {
+            InitializeComponent();
+        }
+
+        public FuPage(string login)
+        {
+            InitializeComponent();
+            this.login = login;
+            //this.prop = prop;
+            quem = login;
+
+            pickerSup.Title = "Selecione um Representante";
+            quemLabel.Text = "Quem: " + login;
+            List<String> represes = new List<String>();
+
+            SqlConnection conn = new BancoAzure().abrir();
+            string sql = "SELECT Nome from Usuarios order by Nome asc";
+            SqlDataReader reader = new BancoAzure().consultar(sql, conn);
+            string errosql = new BancoAzure().consultarErros(sql, conn);
+            represes.Add("");
+            if (reader != null && reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    represes.Add(reader.GetString(0));
+                }
+            }
+            new BancoAzure().fechar(conn);
+            foreach (string repres in represes)
+            {
+                pickerSup.Items.Add(repres);
+            }
+        }
+
+        private void pickerSelected(object sender, EventArgs e)
+        {
+            sup = pickerSup.Items[pickerSup.SelectedIndex].ToString();
+            //DisplayAlert(sup, "Foi o item Selecionado", "OK");
+
+            //DisplayAlert(quem, "Foi o item Selecionado", "OK");
+        }
+
+        private void clicadoBotao(object sender, EventArgs e)
+        {
+            string prop = propec.Text;
+            SqlConnection conn = new BancoAzure().abrir();
+            string sql = "SELECT UFO,UFD,cod_cliente,nom_cliente,Representante " +
+                "from PROPOSTAS where CodProp = '" + prop + "'";
+            SqlDataReader reader = new BancoAzure().consultar(sql, conn);
+            string errosql = new BancoAzure().consultarErros(sql, conn);
+
+            if (reader != null && reader.HasRows)
+            {
+                Proposta proposta = null;
+                while (reader.Read())
+                {
+                    string codProp = prop;
+                    string ufo = reader.GetString(0);
+                    string ufd = reader.GetString(1);
+                    string codCliente = reader.GetString(2);
+                    string nomeCliente = reader.GetString(3);
+                    string repres = reader.GetString(4);
+                    proposta = new Proposta(codProp, ufo, ufd, codCliente, nomeCliente, repres);
+                }
+                reader.Close();
+                if (proposta != null)
+                {
+                    prop = prop.ToUpper();
+                    string str = DateTime.Now.ToString();
+                    DateTime dataFup = DateTime.Parse(str, new CultureInfo("en-US"));
+
+                    sql = "INSERT INTO PropostasFUP" +
+                        "(CodProp,DataFUP,DataAlarme,SetAlarme,FUP,Quem,QuemSup,cod_cliente,UFO,UFD,Representante,nom_cliente)" +
+                        " values('" + prop + "','" + dataFup + "','" + dataFup + "','OFF','" + fup.Text + "','" + quem + "','" + sup + "'," +
+                        "'" + proposta.CodCliente + "','" + proposta.Ufo + "','" + proposta.Ufd + "','" + proposta.Repres + "','" + proposta.NomeCliente + "');";
+
+                    //errosql = new BancoAzure().consultarErros(sql, conn);
+                    new BancoAzure().executar(sql, conn);
+                }
+
+                DisplayAlert("Incluido", "FUP incluido com Sucesso", "OK");
+            }
+            else
+            {
+                erro = "Proposta invalida";
+                DisplayAlert("", erro, "OK");
+            }
+            fup.Text = "";
+            new BancoAzure().fechar(conn);
+            //++count;
+            //((Button)sender).Text = $"{count}";
+        }
+    }
+}
---
+++ FupApp/FupApp/FupApp/Login.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FupApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            // Define o binding context
+            this.BindingContext = this;
+            // Define a propriedade
+            this.IsBusy = false;
+            //Define o evento Click do botão de login
+            BtnLogin.Clicked += BtnLogin_Clicked;
+        }
+
+        private string loginPost;
+        private string senhaPost;
+        private string erro;
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            /*
+
+            senhaPost = senha.Text;
+            loginPost = login.Text;
+            SqlConnection conn = new BancoAzure().abrir();
+            string sql = "SELECT Senha,Nome,Idx,new_cod_repres FROM Usuarios WHERE Nome = '" + loginPost + "' AND Senha = '" + senhaPost + "'";
+
+            SqlDataReader reader = new BancoAzure().consultar(sql, conn);
+
+
+            reader.Read();
+
+            //if ((int)Session["tries"] < 5)
+            //{
+            if (reader.HasRows)
+            {
+                String senha = reader.GetString(0);
+                if (senha == senhaPost)
+                {
+                    string nome = reader.GetString(1);
+                    double idx = reader.GetDouble(2);
+                    string codRepres = reader.GetString(3);
+                    
+            login = nome;
+                    */
+
+            this.IsBusy = true;
+            //MainPage page = new MainPage(nome);
+            MainPage page = new MainPage("SimiSys");
+            Application.Current.MainPage = new NavigationPage(page);
+
+            /*
+
+                    //Session["nome"] = nome;
+                    //Session["idx"] = idx;
+                    //Session["codRepres"] = codRepres;
+
+                    //Session["firstJ"] = 1;
+                    //Session["first"] = 1;
+
+                    ////DEFINE A KEY DO USUARIO
+                    //int key = 1;
+                    //if (idx <= 25) key = 2;
+                    //if (idx <= 24) key = 3;
+                    //if (idx <= 20) key = 5;
+
+                    //if (idx <= 15) key = 7;
+                    //if (idx <= 10) key = 8;
+
+                    //if (nome == "SimiSys") key = 11;
+
+                    //Session["key"] = key;
+                }
+                else
+                {
+                    //Session["tries"] = (int)Session["tries"] + 1;
+                    //erro = "Dados de login incorretos (" + Session["tries"] + "/5)";
+                    erro = "Dados de login incorretos.";
+
+                }
+            }
+            else
+            {
+                //Session["tries"] = (int)Session["tries"] + 1;
+                //erro = "Dados de login incorretos (" + Session["tries"] + "/5)";
+                erro = "Dados de login incorretos.";
+            }
+            Console.Write(erro);
+        }
+        //else
+        //{
+        //    erro = "Tentativas excedidas";
+        //}
+        //Dá uma pausa de ~valor~ segundos
+        async Task DandoUmTempo(int valor)
+        {
+            await Task.Delay(valor);
+        }
+
+    */
+        }
+    }
+}
