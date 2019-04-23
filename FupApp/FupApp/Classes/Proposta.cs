4355cb58eeb24ea39fc0f0a5f5cd70120ac17b59
Verkans <victor@setupnet.com.br>
2019-04-23 17:03:36 -0300

    Fup com os followups aparecendo MASSA
    ta mt daora isso

---
+++ FupApp/FupApp/Classes/Proposta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FupApp.Classes
+{
+    class Proposta
+    {
+        private string codProp;
+        private string ufo;
+        private string ufd;
+        private string codCliente;
+        private string nomeCliente;
+        private string repres;
+
+        public Proposta(string codProp, string ufo, string ufd, string codCliente, string nomeCliente, string repres)
+        {
+            this.codProp = codProp;
+            this.ufo = ufo;
+            this.ufd = ufd;
+            this.codCliente = codCliente;
+            this.nomeCliente = nomeCliente;
+            this.repres = repres;
+        }
+
+        public string CodProp { get => codProp; set => codProp = value; }
+        public string Ufo { get => ufo; set => ufo = value; }
+        public string Ufd { get => ufd; set => ufd = value; }
+        public string CodCliente { get => codCliente; set => codCliente = value; }
+        public string NomeCliente { get => nomeCliente; set => nomeCliente = value; }
+        public string Repres { get => repres; set => repres = value; }
+    }
+}
---
+++ FupApp/FupApp/FuPage.xaml.cs
@@ -0,0 +1,236 @@
+﻿using FupApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FupApp
+{
+    public partial class FuPage : ContentPage
+    {
+        private Picker picker;
+        private string login;
+        private int count = 0;
+        private string erro;
+        private string quem;
+        private string sup;
+        //private string prop;
+
+        public FuPage()
+        {
+            InitializeComponent();
+            //NavigationPage.SetTitleIcon(this, "imgs/syss.png");
+        }
+
+        public FuPage(string login)
+        {
+            InitializeComponent();
+            //NavigationPage.SetTitleIcon(this, "imgs/syss.png");
+            this.login = login;
+            //this.prop = prop;
+            quem = login;
+
+            pickerSup.Title = "Selecione um Representante";
+            quemLabel.Text = login;
+            List<String> represes = new List<String>();
+
+            SqlConnection conn = new BancoAzure().abrir();
+            string sql = "SELECT Nome from Usuarios order by Nome asc";
+            SqlDataReader reader = new BancoAzure().consultar(sql, conn);
+            string errosql = new BancoAzure().consultarErros(sql, conn);
+            represes.Add("");
+            if (reader != null && reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    represes.Add(reader.GetString(0));
+                }
+            }
+            new BancoAzure().fechar(conn);
+            foreach (string repres in represes)
+            {
+                pickerSup.Items.Add(repres);
+            }
+        }
+
+        private void pickerSelected(object sender, EventArgs e)
+        {
+            sup = pickerSup.Items[pickerSup.SelectedIndex].ToString();
+            //DisplayAlert(sup, "Foi o item Selecionado", "OK");
+
+            //DisplayAlert(quem, "Foi o item Selecionado", "OK");
+        }
+
+        private void clicadoBotao(object sender, EventArgs e)
+        {
+            string prop = propec.Text;
+            SqlConnection conn = new BancoAzure().abrir();
+            string sql = "SELECT UFO,UFD,cod_cliente,nom_cliente,Representante " +
+                "from PROPOSTAS where CodProp = '" + prop + "'";
+            SqlDataReader reader = new BancoAzure().consultar(sql, conn);
+            string errosql = new BancoAzure().consultarErros(sql, conn);
+
+            if (reader != null && reader.HasRows)
+            {
+                Proposta proposta = null;
+                while (reader.Read())
+                {
+                    string codProp = prop;
+                    string ufo = reader.GetString(0);
+                    string ufd = reader.GetString(1);
+                    string codCliente = reader.GetString(2);
+                    string nomeCliente = reader.GetString(3);
+                    string repres = reader.GetString(4);
+                    proposta = new Proposta(codProp, ufo, ufd, codCliente, nomeCliente, repres);
+                }
+                reader.Close();
+                if (proposta != null)
+                {
+                    prop = prop.ToUpper();
+                    string str = DateTime.Now.ToString();
+                    //DateTime dataFup = DateTime.Parse(str, new CultureInfo("en-US"));
+                    string[] strA = str.Split('/');
+                    string dataFup = strA[1]+"/"+strA[0]+"/"+strA[2];
+
+                    sql = "INSERT INTO PropostasFUP" +
+                        "(CodProp,DataFUP,DataAlarme,SetAlarme,FUP,Quem,QuemSup,cod_cliente,UFO,UFD,Representante,nom_cliente)" +
+                        " values('" + prop + "','" + dataFup + "','" + dataFup + "','OFF','" + fup.Text + "','" + quem + "','" + sup + "'," +
+                        "'" + proposta.CodCliente + "','" + proposta.Ufo + "','" + proposta.Ufd + "','" + proposta.Repres + "','" + proposta.NomeCliente + "');";
+
+                    //errosql = new BancoAzure().consultarErros(sql, conn);
+                    new BancoAzure().executar(sql, conn);
+                }
+
+                DisplayAlert("Incluido", "FUP incluido com Sucesso", "OK");
+            }
+            else
+            {
+                erro = "Proposta invalida";
+                DisplayAlert("", erro, "OK");
+            }
+            fup.Text = "";
+            new BancoAzure().fechar(conn);
+            //++count;
+            //((Button)sender).Text = $"{count}";
+        }
+
+
+
+        private void pesqFups(object sender, EventArgs e)
+        {
+            fupsProp.Children.Clear();
+            string prop = propec.Text;
+            SqlConnection conn = new BancoAzure().abrir();
+            string sql = "SELECT CodProp,FUP,DataFUP,Quem,QuemSup " +
+                "from PropostasFUP where CodProp = '" + prop + "'";
+            SqlDataReader reader = new BancoAzure().consultar(sql, conn);
+            string errosql = new BancoAzure().consultarErros(sql, conn);
+
+            if (reader != null && reader.HasRows)
+            {
+                List<FollowUp> fupa = new List<FollowUp>();
+                FollowUp fup = null;
+                while (reader.Read())
+                {
+                    string codProp = reader.GetString(0);
+                    string fups = reader.GetString(1);
+                    DateTime dataFup = reader.GetDateTime(2);
+                    string quem = reader.GetString(3);
+                    string quemSup = reader.GetString(4);
+                    //codProp += ", " + dataFup.ToString() + ", Quem:" + quem + ", QuemSup:" + quemSup;
+                    fup = new FollowUp(codProp, fups, dataFup, quem, quemSup);
+                    fupa.Add(fup);
+                }
+                reader.Close();
+                if (fupa.Count > 0)
+                {
+                    foreach (FollowUp f in fupa)
+                    {
+
+                        Grid gridFups = new Grid
+                        {
+                            //BackgroundColor = Color.FromHex("#222")
+                            ColumnSpacing = 1,
+                            RowSpacing = 1
+                        };
+                        gridFups.RowDefinitions.Add(new RowDefinition { Height = new GridLength(30) });
+                        gridFups.RowDefinitions.Add(new RowDefinition { Height = new GridLength(40) });
+                        gridFups.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                        gridFups.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                        gridFups.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                        gridFups.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                        gridFups.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                        Label ldata = new Label
+                        {
+                            Text = f.Data.ToString(),
+                            FontSize = 15,
+                            //BackgroundColor = Color.FromHex("#777")
+                            Margin = new Thickness(3, 3, 0, 0)
+
+                        };
+                        gridFups.Children.Add(ldata, 1, 0);
+                        Grid.SetColumnSpan(ldata, 2);
+
+                        Label lquem = new Label
+                        {
+                            Text = "Quem: " + f.Quem,
+                            FontSize = 15,
+                            //BackgroundColor = Color.FromHex("#777")
+                            Margin = new Thickness(3, 3, 0, 0)
+                        };
+                        gridFups.Children.Add(lquem, 0, 1);
+                        Grid.SetColumnSpan(lquem, 2);
+
+                        Label lquemsup = new Label
+                        {
+                            Text = "Sup: " + f.QuemSup,
+                            FontSize = 15,
+                            //BackgroundColor = Color.FromHex("#777")
+                            Margin = new Thickness(3, 3, 0, 0)
+                        };
+                        gridFups.Children.Add(lquemsup, 2, 1);
+                        Grid.SetColumnSpan(lquemsup, 2);
+
+                        Label lfup = new Label
+                        {
+                            Text = f.Fup,
+                            FontSize = 20,
+                            //TextColor = Color.FromHex(""),
+                            //BackgroundColor = Color.FromHex("#777"),
+                            Margin = new Thickness(10, 10, 10, 20)
+                        };
+                        gridFups.Children.Add(lfup, 0, 2);
+                        Grid.SetColumnSpan(lfup, 4);
+
+                        Label line = new Label
+                        {
+                            HorizontalOptions = LayoutOptions.Fill,
+                            FontSize = 1,
+                            BackgroundColor = Color.FromHex("#888")
+                        };
+                        
+                        fupsProp.Children.Add(line);
+                        fupsProp.Children.Add(gridFups);
+                    }
+                }
+                
+            }
+            else
+            {
+                erro = "Proposta invalida";
+                DisplayAlert("", erro, "OK");
+            }
+            new BancoAzure().fechar(conn);
+            //++count;
+            //((Button)sender).Text = $"{count}";
+        }
+
+
+
+    }
+}
